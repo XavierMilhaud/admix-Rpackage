9c6818985edf82381d112592729b5b9cee75ba45
XavierMilhaud <xavier.milhaud.research@gmail.com>
2021-05-31 11:49:40 +0200

    Update galaxies dataset

--- R/allGalaxies.R
+++ R/allGalaxies.R
@@ -1,10 +1,11 @@
-#' Four galaxies measurements of heliocentric velocities (Carina, Sextans, Sculptor, Fornax)
+#' Four galaxies (Carina, Sextans, Sculptor, Fornax) measurements of heliocentric velocities from SIMBAD astronomical database
 #'
-#' @format A data frame with ... rows and .. variables:
+#' @format An evolving data frame of velocities for 4 dSph galaxies; namely Carina, Sextans, Sculptor and Fornax.
+#'         Currently contains 8,862 rows and 3 columns, with information on:
 #' \describe{
-#'   \item{Target}{The machine}
-#'   \item{HV}{The value of heliocentric velocity}
+#'   \item{Target}{Target identification; Galaxy-ID number}
+#'   \item{HV}{Weighted mean Heliocentric rest frame velocity}
 #'   \item{Name}{The name of the galaxy}
 #' }
-#' @source \url{https://iopscience.iop.org/article/10.1088/0004-6256/137/2/3100}
+#' @source \url{https://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=J/AJ/137/3100/stars&-out.max=50&-out.form=HTML%20Table&-out.add=_r&-out.add=_RAJ,_DEJ&-out.add=_RA%2a-c.eq,_DE%2a-c.eq&-sort=_r&-oc.form=sexa}
 "allGalaxies"
