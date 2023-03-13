f874683d72442555c1a648aa6a02834b56fedd32
XavierMilhaud <xavier.milhaud.research@gmail.com>
2023-03-13 10:35:17 +0100

    Implement (with commentary at the moment) the green light criterion for testing purpose.

--- R/IBM_test_H0.R
+++ R/IBM_test_H0.R
@@ -20,13 +20,15 @@
 #' @param sim_U Random draws of the inner convergence part of the contrast as defined in the IBM approach (see 'Details' below).
 #' @param n_sim_tab Number of simulated gaussian processes used in the tabulation of the inner convergence distribution in the IBM approach.
 #' @param min_size (default to NULL) In the k-sample case, useful to provide the minimal size among all samples. Otherwise, useless.
+#' @param conf.level The confidence level of the 2-samples test, i.e. the quantile level to which the test statistic is compared.
 #' @param parallel (default to FALSE) Boolean to indicate whether parallel computations are performed (speed-up the tabulation).
 #' @param n_cpu (default to 2) Number of cores used when parallelizing.
 #'
 #' @details See the paper presenting the IBM approach at the following HAL weblink: https://hal.archives-ouvertes.fr/hal-03201760
 #'
-#' @return A list of four elements, containing : 1) the test statistic value; 2) the rejection decision; 3) the p-value of the
-#'         test, and 4) the estimated weights of the unknown component for each of the two admixture models.
+#' @return A list of five elements, containing : 1) the test statistic value; 2) the rejection decision; 3) the p-value of the
+#'         test, 4) the estimated weights of the unknown component for each of the two admixture models, 5) the simulated distribution
+#'         of the inner convergence regime (useful to perform the test when comparing to the extreme quantile of this distribution).
 #'
 #' @examples
 #' \donttest{
@@ -45,60 +47,93 @@
 #' list.param <- list(f1 = NULL, g1 = list(mean = 2, sd = 0.7),
 #'                    f2 = NULL, g2 = list(mean = 3, sd = 1.2))
 #' IBM_test_H0(sample1=X.sim,sample2=Y.sim,known.p=NULL, comp.dist=list.comp,comp.param=list.param,
-#'             sim_U = NULL, n_sim_tab = 6, min_size=NULL, parallel=FALSE, n_cpu=2)
+#'             sim_U=NULL, n_sim_tab=6, min_size=NULL, conf.level = 0.95, parallel=FALSE, n_cpu=2)
+#'
+#' ####### Under the alternative H1 :
+#' ## Simulate data:
+#' list.comp <- list(f1 = "norm", g1 = "norm",
+#'                   f2 = "norm", g2 = "norm")
+#' list.param <- list(f1 = list(mean = 1, sd = 1), g1 = list(mean = 2, sd = 0.7),
+#'                    f2 = list(mean = 2, sd = 1), g2 = list(mean = 3, sd = 1.2))
+#' X.sim <- rsimmix(n= 1100, unknownComp_weight=0.85, comp.dist = list(list.comp$f1,list.comp$g1),
+#'                  comp.param = list(list.param$f1, list.param$g1))$mixt.data
+#' Y.sim <- rsimmix(n= 1200, unknownComp_weight=0.75, comp.dist = list(list.comp$f2,list.comp$g2),
+#'                  comp.param = list(list.param$f2, list.param$g2))$mixt.data
+#' list.comp <- list(f1 = NULL, g1 = "norm",
+#'                   f2 = NULL, g2 = "norm")
+#' list.param <- list(f1 = NULL, g1 = list(mean = 2, sd = 0.7),
+#'                    f2 = NULL, g2 = list(mean = 3, sd = 1.2))
+#' IBM_test_H0(sample1=X.sim,sample2=Y.sim,known.p=NULL, comp.dist=list.comp,comp.param=list.param,
+#'             sim_U=NULL, n_sim_tab=6, min_size=NULL, conf.level = 0.95, parallel=FALSE, n_cpu=2)
 #' }
 #'
 #' @author Xavier Milhaud <xavier.milhaud.research@gmail.com>
 #' @export
 
 IBM_test_H0 <- function(sample1, sample2, known.p = NULL, comp.dist = NULL, comp.param = NULL, sim_U = NULL,
-                        n_sim_tab = 50, min_size = NULL, parallel = FALSE, n_cpu = 4)
+                        n_sim_tab = 50, min_size = NULL, conf.level = 0.95, parallel = FALSE, n_cpu = 2)
 {
   ## Estimate the proportions of the mixtures:
-  estim <- IBM_estimProp(sample1 = sample1, sample2 = sample2, known.prop = known.p, comp.dist = comp.dist,
-                         comp.param = comp.param, with.correction = FALSE, n.integ = 1000)
-
-  if (is.null(min_size)) {
-    sample.size <- min(length(sample1), length(sample2))
-  } else {
-    sample.size <- min_size
+  estim <- try(suppressWarnings(IBM_estimProp(sample1 = sample1, sample2 = sample2, known.prop = known.p, comp.dist = comp.dist,
+                                              comp.param = comp.param, with.correction = FALSE, n.integ = 1000)),
+               silent = TRUE)
+  count_error <- 0
+  while ((inherits(x = estim, what = "try-error", which = FALSE)) & (count_error < 3)) {
+    estim <- NULL
+    estim <- try(suppressWarnings(IBM_estimProp(sample1 = sample1, sample2 = sample2, known.prop = known.p, comp.dist = comp.dist,
+                                                comp.param = comp.param, with.correction = FALSE, n.integ = 1000)),
+                 silent = TRUE)
+    count_error <- count_error + 1
   }
-  contrast_val <- sample.size * IBM_empirical_contrast(par = estim[["prop.estim"]], fixed.p.X = estim[["p.X.fixed"]], sample1 = sample1, sample2 = sample2,
-                                                      G = estim[["integ.supp"]], comp.dist = comp.dist, comp.param = comp.param)
-  ## Identify boolean 'green light' criterion to known whether we need to perform the test with stochastic integral tabulation:
-  # green_light <- IBM_greenLight_criterion(estim.obj = estim, sample1 = sample1, sample2 = sample2, comp.dist = comp.dist,
-  #                                        comp.param = comp.param, min_size = min_size, alpha = 0.05)
-  # if (green_light) {
-  #   if (is.null(sim_U)) {
-  #     tab_dist <- IBM_tabul_stochasticInteg(n.sim = n_sim_tab, n.varCovMat = 50, sample1 = sample1, sample2 = sample2, min_size = min_size,
-  #                                          comp.dist = comp.dist, comp.param = comp.param, parallel = parallel, n_cpu = n_cpu)
-  #     sim_U <- tab_dist$U_sim
-  #     contrast_val <- tab_dist$contrast_value
-  #   }
-  #   reject.decision <- contrast_val > quantile(sim_U, 0.95)
-  #   CDF_U <- ecdf(sim_U)
-  #   p_value <- 1 - CDF_U(contrast_val)
-  # } else {
-  #   reject.decision <- TRUE
-  #   p_value <- 1e-16
-  # }
+  if (inherits(x = estim, what = "try-error", which = FALSE)) {
+    estim.weights <- 100
+    contrast_val <- NA
+  } else {
+    estim.weights <- estim[["prop.estim"]]
+    if (is.null(min_size)) {
+      sample.size <- min(length(sample1), length(sample2))
+    } else {
+      sample.size <- min_size
+    }
+    contrast_val <- sample.size * IBM_empirical_contrast(par = estim.weights, fixed.p.X = estim[["p.X.fixed"]], sample1 = sample1, sample2 = sample2,
+                                                         G = estim[["integ.supp"]], comp.dist = comp.dist, comp.param = comp.param)
+    ## Identify boolean 'green light' criterion to known whether we need to perform the test with stochastic integral tabulation:
+    # green_light <- IBM_greenLight_criterion(estim.obj = estim, sample1 = sample1, sample2 = sample2, comp.dist = comp.dist,
+    #                                        comp.param = comp.param, min_size = min_size, alpha = 0.05)
+    # if (green_light) {
+    #   if (is.null(sim_U)) {
+    #     tab_dist <- IBM_tabul_stochasticInteg(n.sim = n_sim_tab, n.varCovMat = 50, sample1 = sample1, sample2 = sample2, min_size = min_size,
+    #                                          comp.dist = comp.dist, comp.param = comp.param, parallel = parallel, n_cpu = n_cpu)
+    #     sim_U <- tab_dist$U_sim
+    #     contrast_val <- tab_dist$contrast_value
+    #   }
+    #   reject.decision <- contrast_val > quantile(sim_U, 0.95)
+    #   CDF_U <- ecdf(sim_U)
+    #   p_value <- 1 - CDF_U(contrast_val)
+    # } else {
+    #   reject.decision <- TRUE
+    #   p_value <- 1e-16
+    # }
+  }
 
   ## Earn computation time using this soft version of the green light criterion:
-  if (any(abs(estim[["prop.estim"]]) > 2)) {
+  if (any(abs(estim.weights) > 1)) {
     reject.decision <- TRUE
     p_value <- 1e-16
+    sim_U <- NA
   } else {
     if (is.null(sim_U)) {
-      tab_dist <- IBM_tabul_stochasticInteg(n.sim = n_sim_tab, n.varCovMat = 50, sample1 = sample1, sample2 = sample2, min_size = min_size,
-                                           comp.dist = comp.dist, comp.param = comp.param, parallel = parallel, n_cpu = n_cpu)
+      tab_dist <- IBM_tabul_stochasticInteg(n.sim = n_sim_tab, n.varCovMat = 60, sample1 = sample1, sample2 = sample2, min_size = min_size,
+                                            comp.dist = comp.dist, comp.param = comp.param, parallel = parallel, n_cpu = n_cpu)
       sim_U <- tab_dist$U_sim
       contrast_val <- tab_dist$contrast_value
     }
-    reject.decision <- contrast_val > stats::quantile(sim_U, 0.95)
+    reject.decision <- contrast_val > stats::quantile(sim_U, conf.level)
     CDF_U <- stats::ecdf(sim_U)
     p_value <- 1 - CDF_U(contrast_val)
   }
 
-  return( list(test.stat = contrast_val, decision = reject.decision, p_val = p_value, weights = estim[["prop.estim"]]) )
+  return( list(confidence_level = conf.level, decision = reject.decision, p_val = p_value,
+               test.stat = contrast_val, weights = estim.weights, tabulated_dist = sim_U) )
 }
 
